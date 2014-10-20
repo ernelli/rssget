02d156bd749c19c8537682d96fa4567c86e1d915
Mattias Ernelli <mattias@ernelli.com>
2014-10-20 08:20:18 +0200

    Improved config handling, added config section for showrss.info

--- rssget.js
+++ rssget.js
@@ -2,21 +2,44 @@ var xml2js = require('xml2js');
 var fs = require('fs');
 var request = require('request');
 var http = require('http');
+var querystring = require('querystring');
 var Transmission = require('transmission');
 
+function mixin(a, b) {
+    for(i in b) {
+        a[i] = b[i];
+    }
+    return a;
+}
+
 var config = {
-    url: "",
-    transmission: {}
+    showrss: {
+        user_id: "",
+        hd: "null",
+        proper: "null"
+    },
+    transmission: {
+        host: "localhost",
+        user: "transmission",
+        password: ""}
 };
 
 try {
-    config = JSON.parse(fs.readFileSync("config.json"));
-} catch(e) {
-    console.log("config file missing or bad; generating default config: ", e);
-    //fs.writeFileSync("config.json", JSON.stringify(config));
+    var jsonConfig = fs.readFileSync("config.json")
+} catch(e1) {
+    console.log("config file missing or bad; generating default config: ", e1);
+    fs.writeFileSync("config.json", JSON.stringify(config));
     process.exit(1);
 }
 
+try {
+    fileConfig = JSON.parse(jsonConfig);
+} catch(e2) {
+    console.log("bad config file: ", e2);
+    process.exit(1);
+}
+
+mixin(config, fileConfig);
 
 console.log("using config: ", config);
 
@@ -24,8 +47,12 @@ var xmlparser = new xml2js.Parser();
 
 var tm = new Transmission(config.transmission);
 
-function getrss() {
-request({ uri: config.url, method: "GET"}, function(err, res, xml) {
+function getrss(config) {
+    var uri = "http://showrss.info/rss.php?" + querystring.stringify(config);
+
+    console.log("get uri: " + uri);
+
+    request({ uri: uri, method: "GET"}, function(err, res, xml) {
     if(err) {
         console.log("rssget failed: ", err);
         return;
@@ -72,6 +99,6 @@ tm.active(function(err, res) {
     }
 
     console.log("active: ", res);
-    getrss();
+    getrss(config.showrss);
 });
 
